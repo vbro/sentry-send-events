216f8e591dfa76d231441335edbbfa32fbdc2823
Valery Brobbey <valery.brobbey@sentry.io>
2024-06-05 12:57:41 -0700

    install Sentry, install tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import * as Sentry from "@sentry/vue";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +11,18 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_DEFAULT_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost"],
+  // Session Replay
+  replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 app.mount("#app");
