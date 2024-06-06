6d1cc3a65743a65bc27835f845a3e0b9ea3f7ce7
Valery Brobbey <valery.brobbey@sentry.io>
2024-06-05 21:32:08 -0700

    create SendSentryEvents page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
+import SendSentryEvents from "../views/SendSentryEvents.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: SendSentryEvents,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/SendSentryEvents.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const org = ref<string>(import.meta.env.VITE_SENTRY_ORG);
+const project = ref<string>(import.meta.env.VITE_SENTRY_PROJECT);
+const dsn = ref<string>(import.meta.env.VITE_SENTRY_DSN);
+</script>
+
+<template>
+  <div class="container mx-auto max-w-screen-lg px-4">
+    <div class="w-full text-center">
+      <div class="mx-auto w-full py-4 text-4xl text-green-700">Send events to Sentry</div>
+      <div class="mx-auto w-full border-y py-3 text-gray-500">
+        <div>Project = {{ org ?? "?unknown?" }}</div>
+        <div>Organization = {{ project ?? "?unknown?" }}</div>
+        <div>DSN = {{ dsn }}</div>
+      </div>
+    </div>
+  </div>
+</template>
