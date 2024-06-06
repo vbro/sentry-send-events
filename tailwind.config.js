155467f344a0757e226018fb859feb0f1cd7f762
Valery Brobbey <valery.brobbey@sentry.io>
2024-06-05 19:43:40 -0700

    install autoprefixer, postcss, prettier-plugin-tailwindcss update prettier and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
