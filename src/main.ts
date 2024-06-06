bc679ea3717fdf1a603d2e03f3790bd29cff4020
Valery Brobbey <valery.brobbey@sentry.io>
2024-06-05 19:40:08 -0700

    remove hello world components

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { initSentry } from "./utils/sentryUtils";
+
+import "../index.css";
+import * as Sentry from "@sentry/vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-initSentry(app);
+Sentry.init({
+  app: app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost"],
+  // Session Replay
+  replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 app.mount("#app");
-
-export default app;
